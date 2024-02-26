59bb2bf9a83b86231e03669d8871b07482443c7c
Rupkatha <rupkatha6005@gmail.com>
2024-02-27 01:46:46 +0600

    feat: nextui added

---
+++ hire-hub/src/app/provider.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import * as React from "react";
+
+import { NextUIProvider } from "@nextui-org/react";
+
+export default function Provider({ children }: { children: React.ReactNode }) {
+  return <NextUIProvider>{children}</NextUIProvider>;
+}
