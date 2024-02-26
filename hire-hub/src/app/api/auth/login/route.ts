71bd21cedb4641a16d378987508432fdaf8cbaca
HypnoSphynx <123151627+HypnoSphynx@users.noreply.github.com>
2024-02-27 02:01:38 +0600

    signupfixed

--- hire-hub/src/app/api/auth/login/route.ts
+++ hire-hub/src/app/api/auth/login/route.ts
@@ -1,50 +1,52 @@
-import vine, { errors } from "@vinejs/vine";
 import { NextRequest, NextResponse } from "next/server";
 import { connect } from "@/database/mongo.config";
-import ErrorReporter from "@/validator/ErrorReporter";
-import { loginSchema } from "@/validator/authValidationSchema";
 import { User } from "@/models/User";
+import { registerSchema } from "@/validator/authValidationSchema";
+import vine, { errors } from "@vinejs/vine";
+import ErrorReporter from "@/validator/ErrorReporter";
 import bcrypt from "bcryptjs";
 
-
+interface UserPayload {
+  name: string;
+  email: string;
+  password: string;
+  city: string;
+  country: string;
+  number: string;
+  age: string;
+}
 
 connect();
-
 export async function POST(request: NextRequest) {
   try {
-    const body = await request.json();
+    const body: UserPayload = await request.json();
     vine.errorReporter = () => new ErrorReporter();
-    const validator = vine.compile(loginSchema);
+    const validator = vine.compile(registerSchema);
     const output = await validator.validate(body);
-    const user = await User.findOne({ email: output.email });
-    if (user) {
-      const checkPassword = bcrypt.compareSync(output.password!, user.password);
-      console.info("The checkpassword is", checkPassword);
-      if (checkPassword) {
+    try {
+      const user = await User.findOne({ email: output.email });
+      if (user) {
         return NextResponse.json(
-          { status: 200, message: "Logged in successfully!" },
+          {
+            status: 400,
+            errors: {
+              email: "Email is already used.",
+            },
+          },
+          { status: 200 }
+        );
+      } else {
+        // * To Hash the password
+        const salt = bcrypt.genSaltSync(10);
+        output.password = bcrypt.hashSync(output.password, salt);
+        await User.create(output);
+        return NextResponse.json(
+          { status: 200, msg: "User Created successfully!" },
           { status: 200 }
         );
       }
-      return NextResponse.json(
-        {
-          status: 400,
-          errors: {
-            password: "Password is Incorrect. Please check your password.",
-          },
-        },
-        { status: 200 }
-      );
-    } else {
-      return NextResponse.json(
-        {
-          status: 400,
-          errors: {
-            email: "Couldn't found any user with this email. Please Check your email",
-          },
-        },
-        { status: 200 }
-      );
+    } catch (error) {
+      return NextResponse.json({ error }, { status: 500 });
     }
   } catch (error) {
     if (error instanceof errors.E_VALIDATION_ERROR) {
--- hire-hub/src/validator/authValidationSchema.ts
+++ hire-hub/src/validator/authValidationSchema.ts
@@ -4,6 +4,10 @@ export const registerSchema = vine.object({
   name: vine.string().trim().minLength(3).maxLength(30),
   email: vine.string().email(),
   password: vine.string().minLength(6).maxLength(20).confirmed(),
+  city: vine.string(),
+  country: vine.string(),
+  number: vine.string(),
+  age: vine.string(),
 });
 
 export const loginSchema = vine.object({
