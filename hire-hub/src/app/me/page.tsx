d1c0987eb607793e7db5d1f1a7aac389be067d7c
Rupkatha <rupkatha6005@gmail.com>
2024-02-27 01:47:05 +0600

    feat: your profile page added

---
+++ hire-hub/src/app/me/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function page() {
+  // TODO: 1. Find the user (if any)
+  // TODO: 2. Show the user information
+  return (
+    <div className="flex flex-col min-h-screen bg-slate-900 text-white p-12">
+      <h1 className="text-5xl font-semibold">Your Profile</h1>
+    </div>
+  );
+}
